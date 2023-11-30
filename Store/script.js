5f1748b6ca51ae0d5f387ecd89e589637d050dcc
Isaac <s10257760@connect.np.edu.sg>
2023-11-30 09:36:18 +0800

    new file organization

--- Store/script.js
+++ Store/script.js
@@ -1,3 +1,15 @@
 document.getElementById("return").onclick = function () {
     location.href = "../Store/Store.html"
 }
+
+const button = document.querySelector("add-btn")
+button.addEventListener("click", ()=>{
+    increment();
+});
+
+function increment(){
+    let input = document.getElementById("num")
+    input.stepUp;
+
+}
+
