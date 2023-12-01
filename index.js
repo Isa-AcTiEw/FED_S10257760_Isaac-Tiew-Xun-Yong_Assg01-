899254786fea6c697c8f2815342d14a72577d550
Isaac <s10257760@connect.np.edu.sg>
2023-12-01 12:49:05 +0800

    new pop up

---
+++ index.js
@@ -0,0 +1,7 @@
+let popUp = document.getElementsByClassName("modal-hidden")
+function openPopup(){
+    popUp.classList.add("open-Popup")
+}
+function closePopup(){
+    popUp.classList.remove("open-Popup")
+}
